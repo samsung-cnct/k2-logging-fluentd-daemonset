158310dd7dcc078fa60c966a6ca9d32f395f1e32
Deirdre Storck <deirdre.storck@gmail.com>
2017-01-11 11:20:41 -0800

    changed regex to handle PR for changed filename conventions

--- init/plugins/filter_deirdre_test.rb
+++ init/plugins/filter_deirdre_test.rb
@@ -12,43 +12,49 @@ module Fluent
       super
     end
 
-    def get_log_directory(arr)
-      arr.each do |f|
-       if (!f.match /.log\z/) && (!f.match /\A\./)
-        return f
-       end
-      end
-    end
-
-    def get_filename(arr)
-      arr.each do |f|
-        if !f.match /\A\./
-          return f
-        end
-      end
-    end
+    # def get_log_directory(arr)
+    #   arr.each do |f|
+    #    if (!f.match /.log\z/) && (!f.match /\A\./)
+    #     return f
+    #    end
+    #   end
+    # end
+    #
+    # def get_filename(arr)
+    #   arr.each do |f|
+    #     if !f.match /\A\./
+    #       return f
+    #     end
+    #   end
+    # end
 
 
     def filter_stream(tag, es)
       new_es =  MultiEventStream.new
-      entries = Dir.entries("/var/log/containers/")
-      filepath = get_log_directory(entries)
-      log_file = Dir.entries("/var/log/containers/#{filepath}")
-      filename = get_filename(log_file)
+      # entries = Dir.entries("/var/log/containers/")
+      # filepath = get_log_directory(entries)
+      # log_file = Dir.entries("/var/log/containers/#{filepath}")
+      # filename = get_filename(log_file)
 
-      tag_path = tag.gsub "named.var.log.containers.", '/'
-      file = (tag_path.scan  /\.\w+\.\w+\Z/)[0]
-      file[0] = '/'
-      tag_regex = tag_path.gsub /\.\w+\.\w+\Z/ , file
+      # tag_path = tag.gsub "named.var.log.containers.", '/'
+      # file = (tag_path.scan  /\.\w+\.\w+\Z/)[0]
+      # file[0] = '/'
+      # tag_regex = tag_path.gsub /\.\w+\.\w+\Z/ , file
+      # str = "named.var.log.containers.log-generator_default_10.128.76.4.all_logs.txt"
+
+      tag_path = tag.gsub "named.var.log.containers.", ''
+      tag_path == "log-generator_default_10.128.76.4.all_logs.txt"
+      arry = tag_path.scan /[^_]+/
+      /(?<pod_name>[^_]+)_(?<pod_namespace>[^_]+)_(?<pod_ip>\d+\.\d+\.\d+\.\d+)/ =~ tag_path
 
       es.each {|time, record|
-        record['uniquestring'] = {
-          'name'            => 'hatch',
-          'filepath'        => "/#{filepath}/#{filename}",
-          'tag'             => tag_regex,
-          'kube_namespace'  => "#{ENV['POD_NAMESPACE']}",
-          'kube_pod_name'   => "#{ENV['POD_NAME']}",
-          'KUBE_POD_IP'     => "#{ENV['POD_IP']}"
+        record['named_file_info'] = {
+          'name'            => 'hatch'
+        }
+        record['kubernetes'] = {
+          'name'      => pod_name,
+          'namespace' => pod_namespace,
+          'ip'        => pod_ip
         }
         new_es.add(time, record)
       }

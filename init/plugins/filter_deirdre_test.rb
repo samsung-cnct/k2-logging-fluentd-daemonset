cb036b9c66f64ae478f411de388b107e850b351c
Deirdre Storck <deirdre.storck@gmail.com>
2017-01-12 14:57:53 -0800

    updated conf file and and filter to add back in test filter to fluentd system

--- init/plugins/filter_deirdre_test.rb
+++ init/plugins/filter_deirdre_test.rb
@@ -12,46 +12,17 @@ module Fluent
       super
     end
 
-    # def get_log_directory(arr)
-    #   arr.each do |f|
-    #    if (!f.match /.log\z/) && (!f.match /\A\./)
-    #     return f
-    #    end
-    #   end
-    # end
-    #
-    # def get_filename(arr)
-    #   arr.each do |f|
-    #     if !f.match /\A\./
-    #       return f
-    #     end
-    #   end
-    # end
-
-
     def filter_stream(tag, es)
       new_es =  MultiEventStream.new
-      # entries = Dir.entries("/var/log/containers/")
-      # filepath = get_log_directory(entries)
-      # log_file = Dir.entries("/var/log/containers/#{filepath}")
-      # filename = get_filename(log_file)
-
-      # tag_path = tag.gsub "named.var.log.containers.", '/'
-      # file = (tag_path.scan  /\.\w+\.\w+\Z/)[0]
-      # file[0] = '/'
-      # tag_regex = tag_path.gsub /\.\w+\.\w+\Z/ , file
-      # str = "named.var.log.containers.log-generator_default_10.128.76.4.all_logs.txt"
 
       tag_path = tag.gsub "named.var.log.containers.", ''
-      # tag_path == "log-generator_default_10.128.76.4.all_logs.txt"
-      # arry = tag_path.scan /[^_]+/
       /(?<pod_name>[^_]+)_(?<pod_namespace>[^_]+)_(?<pod_ip>\d+\.\d+\.\d+\.\d+)/ =~ tag_path
 
       es.each {|time, record|
         record['named_file_info'] = {
-          'name'            => 'hatch'
+          'name' => 'hatch'
         }
-        record['kube'] = {
+        record['dkube'] = {
           'tag'       => tag,
           'tag_path'  => tag_path,
           'name'      => pod_name,

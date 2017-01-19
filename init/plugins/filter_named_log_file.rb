e62b68fcc6375da93d030284bae8f3df5351d981
Deirdre Storck <deirdre.storck@gmail.com>
2017-01-19 11:26:03 -0800

    changed tag regex to not be sensitive to tag assigned in fluent.conf

--- init/plugins/filter_named_log_file.rb
+++ init/plugins/filter_named_log_file.rb
@@ -15,15 +15,12 @@ module Fluent
     def filter_stream(tag, es)
       new_es =  MultiEventStream.new
 
-      #'named' is current tag set in fluent.conf
-      # if tag is changed, tag_path will need to change to match
-      tag_path = tag.gsub "named.var.log.containers.", ''
-      # /(?<pod_name>[^_]+)_(?<pod_namespace>[^_]+)_(?<pod_ip>\d+\.\d+\.\d+\.\d+)/ =~ tag_path
+      tag_path = tag.gsub /[^\.]+.var.log.containers./, ''
       /(?<folder>[^_]+_[^_]+_\d+\.\d+\.\d+\.\d+_unknown_unknown)\.(?<filename>.+)/ =~ tag_path
       filepath = "/#{folder}/#{filename}"
 
       es.each {|time, record|
-        record['named_file_info'] = {
+        record['named_logfile'] = {
           'filepath' => filepath,
           'filename' => filename
         }

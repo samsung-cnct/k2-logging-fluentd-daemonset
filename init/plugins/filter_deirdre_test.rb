7ad70f1d9f7bef717a0d81ee5d735bb5a795a0f4
Deirdre Storck <deirdre.storck@gmail.com>
2017-01-18 17:00:48 -0800

    added unknown to directory path to match current filepath

--- init/plugins/filter_deirdre_test.rb
+++ init/plugins/filter_deirdre_test.rb
@@ -14,12 +14,12 @@ module Fluent
 
     def filter_stream(tag, es)
       new_es =  MultiEventStream.new
-      
+
       #'named' is current tag set in fluent.conf
       # if tag is changed, tag_path will need to change to match
       tag_path = tag.gsub "named.var.log.containers.", ''
       /(?<pod_name>[^_]+)_(?<pod_namespace>[^_]+)_(?<pod_ip>\d+\.\d+\.\d+\.\d+)/ =~ tag_path
-      /(?<folder>[^_]+_[^_]+_\d+\.\d+\.\d+\.\d+)\.(?<filename>.+)/ =~ tag_path
+      /(?<folder>[^_]+_[^_]+_\d+\.\d+\.\d+\.\d+)_unknown_unknown\.(?<filename>.+)/ =~ tag_path
       filepath = "/#{folder}/#{filename}"
 
       es.each {|time, record|
